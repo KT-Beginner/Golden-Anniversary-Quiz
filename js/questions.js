6109b11b3d86f8662e80d92466699b159c206f0f
KT-Beginner <kevinanddawn@talktalk.net>
2026-08-26 01:12:57 +0100

    break

--- js/questions.js
+++ js/questions.js
@@ -99,7 +99,7 @@ const questions = [
          manualNext: true,   
 
     
-    photoTitle: "🎵 Dancing Queen - ABBA were famous<br>for their extravagant costumes",
+    photoTitle: "🎵 Dancing Queen<b>ABBA were famous for their<br>extravagant costumes",
     photoText: "Swedish law meant costumes could be deducted against tax so long as they were so outrageous they could not possibly be worn on the street",
     },
     { 
